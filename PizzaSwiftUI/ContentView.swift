68f10768eb579929de49b625da7cc56cd2958413
Matt Deuschle <mdeuschle@gmail.com>
2020-03-22 14:11:55 -0700

    first commit

---
+++ PizzaSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PizzaSwiftUI
+//
+//  Created by Matt Deuschle on 3/22/20.
+//  Copyright © 2020 Matt Deuschle. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
